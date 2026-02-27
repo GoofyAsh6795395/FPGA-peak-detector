dd141c42e01a762d67b82bb11246fb9afbf44ad7
GoofyAsh6795395 <goofyash6795395jp@outlook.com>
2026-02-27 15:41:01 +0000

    Add patches on parser

--- VHDL/Command_Processor/parser.vhd
+++ VHDL/Command_Processor/parser.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-
+use IEEE.NUMERIC_STD.all;
 
 entity parser is
 	port(
@@ -8,6 +8,8 @@ entity parser is
 		reset: in STD_LOGIC;
 		command: in STD_LOGIC_VECTOR (7 downto 0);
 		enable: in STD_LOGIC;
+
+		NNN: out STD_LOGIC_VECTOR(11 downto 0);		--BCD encoding is used.
 
 		isANNN: out STD_LOGIC;
 		isL: out STD_LOGIC;
@@ -23,6 +25,8 @@ architecture synth of parser is
 	--So, there are not separated state "ANNN", "L" or "P" in the type declaration.
 	signal current_state, next_state: recorded := idle;
 
+	signal NNN_reg: STD_LOGIC_VECTOR(11 downto 0);
+
 	constant ascii_a: STD_LOGIC_VECTOR (7 downto 0) := x"61";
 	constant ascii_a_cap: STD_LOGIC_VECTOR (7 downto 0) := x"41";
 	constant ascii_l: STD_LOGIC_VECTOR (7 downto 0) := x"6C";
@@ -35,6 +39,8 @@ architecture synth of parser is
 	--signal ANNN_found, L_found, P_found: STD_LOGIC := '0';
 	--signal ANNN_next, L_next, P_next: STD_LOGIC := '0';
 begin
+	NNN <= NNN_reg;
+
 	state_transition_logic:
 	process(enable, command, current_state)
 	begin
@@ -97,7 +103,7 @@ begin
 	--Use registed style output to ensure the success signal "isANNN", "isL" and "isP" are strictly one clock cycle.
 	--Also, to avoid gliches.
 	update:
-	process(clk)
+	process(clk, reset)
 	begin
 		if rising_edge(clk) then
 			if reset = '1' then
@@ -108,35 +114,80 @@ begin
 		end if;
 	end process;
 	
-	output_logic:
-	process(current_state, enable, command)
+	clock_controlled_datapath:
+	process(clk, reset, current_state, enable, command)
+		variable command_bcd: STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
 	begin
-		--Assign first, to avoid latch.
-		isANNN <= '0';
-		isL <= '0';
-		isP <= '0';
-		
-		if enable = '1' then
-			if command = ascii_l or command = ascii_l_cap then
-				isANNN <= '0';
-				isL <= '1';
-				isP <= '0';
-			elsif command = ascii_p or command = ascii_p_cap then
+		if rising_edge(clk) then
+			if reset = '1' then
+				NNN_reg <= (others => '0');	--Use aggregate style assignment.
 				isANNN <= '0';
 				isL <= '0';
-				isP <= '1';
-			end if;
-			
-			if current_state = ANN then
-				if command >= ascii_0 and command <= ascii_9 then
-					isANNN <= '1';
+				isP <= '0';
+			else
+				--Assign first, to keep exactly one cycle pulse output.
+				isANNN <= '0';
+				isL <= '0';
+				isP <= '0';
+		
+				if enable = '1' then
+					--Let's count the cycles to check how output is effected by enable.
+					--At k clocking edge, enable gets high level, start detecting
+					--At k+1 clocking edge, enable gets low, but the output will be seriously cleaned at K+2 clock period.
+					--However, the output is already determined at K+1 cycle, and it will successfully propagate.
+					
+					if command = ascii_l or command = ascii_l_cap then
+						isANNN <= '0';
+						isL <= '1';
+						isP <= '0';
+					elsif command = ascii_p or command = ascii_p_cap then
+						isANNN <= '0';
+						isL <= '0';
+						isP <= '1';
+					end if;
+
+					--Encode BCD code firstly.
+					--Command is encoded by binary ascii code, with bitwidth of 8.
+					--Hint: the last 4 digits of ascii code of a number is coinsidently its binary.
+					command_bcd := command(3 downto 0);
+
+					case current_state is
+						when idle =>
+							NNN_reg <= (others => '0');		--Flush the recorded data if NNN sequence is interrupted.
+						when A =>
+							if command >= ascii_0 and command <= ascii_9 then
+								NNN_reg(11 downto 8) <= command_bcd;
+							else
+								NNN_reg <= (others => '0');	--Flush.
+							end if;
+						when AN =>
+							if command >= ascii_0 and command <= ascii_9 then
+								NNN_reg(7 downto 4) <= command_bcd;
+							else
+								NNN_reg <= (others => '0');	--Flush.
+							end if;
+						when ANN =>
+							if command >= ascii_0 and command <= ascii_9 then
+								isANNN <= '1';
+								isL <= '0';
+								isP <= '0';
+
+								NNN_reg(3 downto 0) <= command_bcd;
+							else
+								NNN_reg <= (others => '0');	--Flush.
+							end if;
+					end case;
+				else				--Not enabled.
+					isANNN <= '0';
 					isL <= '0';
 					isP <= '0';
 				end if;
+
 			end if;
 		end if;
 	end process;
 end architecture;
+
 
 --Log:
 --Firstly drafted on a certain day in early Feburary.
@@ -150,4 +201,19 @@ end architecture;
 --		2. A fully guaranteed up-stream input.
 --	Currently, not sure if the first solution and enable signal couple well or not.
 --	Also, a limited coverage is conducted, enable signal is not considered.
---End;
+--
+--Spotted at 2pm, 27/02/2026:
+--	A output databus is required to tell what's specifically NNN is.
+--	Enable signal is not tested yet.
+--	As mealy machine, the success signal is send immediately once condiction is met.
+--	However, the register will have one cycle latency to read out.
+--	Maybe make the success signal to be also an output of a register will solve, I'll have a try.
+--
+--Modified at 3pm, 27/02/2026:
+--	Correspondingly change the pulse style, and add the NNN output.
+--	Compile successfully, not tested.
+--
+--Commitment at 3.30pm, 27/02/2026
+--	Roughly tested, identified that BCD sequence is reversed.
+--	Already corrected this error and work in order.
+--	More test may be better.
--- VHDL/Command_Processor/scheduler.vhd
+++ VHDL/Command_Processor/scheduler.vhd
@@ -3,12 +3,41 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity scheduler is
 	port(
+		--External control:
+		clk: in STD_LOGIC;
+		reset: in STD_LOGIC;
 		
+		--To parser.
+		isANNN: in STD_LOGIC;
+		isL: in STD_LOGIC;
+		isP: in STD_LOGIC;
+
+		--To transmitter side.
+		data_out: out STD_LOGIC_VECTOR(7 downto 0);
+		print_req: out STD_LOGIC;
+		print_ack: in STD_LOGIC;
+
+		--To data processor
+
+		start: out STD_LOGIC;
+		dataReady: in STD_LOGIC;
+		seqDone: in STD_LOGIC;
+
+		numWords: out STD_LOGIC(11 downto 0);
+		byte: in STD_LOGIC_VECTOR(7 downto 0);
+		dataResults: in STD_LOGIC_VECTOR(55 downto 0);
+		maxIndex: in STD_LOGIC_VECTOR(11 downto 0);
+
+		start: out STD_LOGIC;
+		dataReady: in STD_LOGIC;
+		seqDone: in STD_LOGIC;
 	);
 end entity;
 
 architecture synth of scheduler is
-
+	type stateType is (idle, run, printData, printL, printP);
+	signal current_state, next_state: stateType := idle;
 begin
-
+	state_transition_logic:
+	
 end architecture;
